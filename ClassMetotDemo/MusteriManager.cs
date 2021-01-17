25ae0843b2b60cd41f785e66a1fe4137a1111f49
hilal tekdemir <hilal.tek.demir@hotmail.com>
2021-01-17 23:35:29 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public ArrayList musteris = new ArrayList { };
+        public void Ekle(Musteri musteri)
+        {
+            musteris.Add(musteri);
+            Console.WriteLine(musteri.AdSoyad + " isimli bir müşteri eklenmiştir.");
+        }
+
+        public void Listele()
+        {
+            foreach (Musteri item in musteris)
+            {
+                Console.WriteLine(item.AdSoyad + " isimli müşterinin hesabı "+ item.HesapTuru +" şeklindedir");
+            }
+        }
+
+        public void Silme(Musteri musteri)
+        {
+            musteris.Remove(musteri);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+
+            Musteri m1 = new Musteri();
+            m1.AdSoyad = "Hayrettin Kala";
+            m1.HesapTuru = "Vadesiz Mevduat";
+            musteriManager.Ekle(m1);
+
+            Musteri m2 = new Musteri();
+            m2.AdSoyad = "Saniye Yerli";
+            m2.HesapTuru = "Vadesiz Mevduat";
+            musteriManager.Ekle(m2);
+
+            Console.WriteLine("--------------------------");
+            musteriManager.Listele();
+
+            Console.WriteLine("--------------------------");
+
+            musteriManager.Silme(m1);
+
+            Console.WriteLine("--------------------------");
+            musteriManager.Listele();
+
+            Console.WriteLine("--------------------------");
+        }
+    }
+}
